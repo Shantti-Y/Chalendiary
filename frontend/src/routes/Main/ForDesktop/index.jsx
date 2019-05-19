ca0b7b861dde5849511b6615b5418e72f2764c3c
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-05-19 22:08:31 +0900

    Re-create Main page in Desktop

--- frontend/src/routes/Main/ForDesktop/index.jsx
+++ frontend/src/routes/Main/ForDesktop/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './style.scss';
+
 import moment from 'moment';
+
+import { DEFAULT_TAG_NAME } from '@utils/defaultValues';
 
 class ForDesktop extends React.Component {
 
@@ -8,12 +11,89 @@ class ForDesktop extends React.Component {
     super(props);
   }
 
-
   render(){
+    const {
+      me,
+      currentDate,
+      tags,
+      currentTagId,
+      users,
+      diaries
+    } = this.props;
+
+    const currentTagName = () => {
+      const currentTag = tags.find(tag => tag.id === currentTagId);
+      if (currentTag) {
+        return currentTag.name;
+      } else {
+        return DEFAULT_TAG_NAME;
+      }
+    };
+    const daysInMonth = currentDate.daysInMonth();
+    const members = () => {
+      if(currentTagId === null){
+        return users;
+      }
+
+      return users.filter(user => {
+        return user.tags.map(tag => tag.id).includes(currentTagId)
+      });
+    }
 
     return (
       <div id="for-desktop" className="layout-container">
-        
+        <div id="header">
+          <div className="date-swapper">
+            <span>Left</span>
+            <span>2019/02</span>
+            <span>Right</span>
+            <span>{ currentTagName() }</span>
+          </div>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th className="user-column">Members</th>
+                {[...Array(daysInMonth)].map((_, i) => {
+                    const day = i + 1;
+                    return (
+                      <th key={day}>{ day }</th>
+                    )
+                  })}
+              </tr>
+            </thead>
+            <tbody>
+              {
+                members().map(member => {
+                  return (
+                    <tr key={member.id}>
+                      <td className="user-column">
+                        { member.screenName }
+                      </td>
+                      {
+                        [...Array(daysInMonth)].map((_, i) => {
+                          const day = i + 1;
+                          const diaryDataInDay = diaries.find(item => item.date === moment().date(day).format("YYYY-MM-DD"));
+                          if (diaryDataInDay){
+                            const diariesInDay = diaryDataInDay.diaries.find(diary => diary.user.id === member.id)
+                            return (
+                              <td key={`${member.id}-${day}`}>
+                                {diariesInDay ? diariesInDay.contentText : null}
+                              </td>
+                            )
+                          }else {
+                            return null;
+                          }
+                        })
+                      }
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
--- frontend/src/routes/Main/index.jsx
+++ frontend/src/routes/Main/index.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './style.scss';
 
 import { connect } from 'react-redux';
+import moment from 'moment';
 
-import { Hidden } from '@material-ui/core';
+import { fetchMe } from '@store/actions/me';
+import { fetchUsers } from '@store/actions/user';
+import { searchDiaries } from '@store/actions/diary';
+import { fetchTags } from '@store/actions/tag';
 
 import ForDesktop from '@routes/Main/ForDesktop';
 
@@ -14,14 +18,29 @@ class Main extends React.Component{
   }
 
   componentDidMount(){
-    this.props.onFetchMe();
-    this.props.onFetchTeams();
+    this.props.onInitialize();
   }
 
   render(){
+    const {
+      me,
+      currentDate,
+      tags,
+      currentTagId,
+      users,
+      diaries
+    } = this.props;
+
     return(
       <div id="main">
-        <ForDesktop />
+        <ForDesktop
+          me={me}
+          currentDate={currentDate}
+          tags={tags}
+          currentTagId={currentTagId}
+          users={users}
+          diaries={diaries}
+          />
       </div>
     )
   }
@@ -30,10 +49,20 @@ class Main extends React.Component{
 const mapStateToProps = state => ({
   me: state.me.me,
   currentDate: state.date.currentDate,
-
+  users: state.user.users,
+  tags: state.tag.tags,
+  currentTagId: state.tag.currentTagId,
+  diaries: state.diary.diaries
 });
 
 const mapDispatchToProps = dispatch => ({
+  onInitialize: () => {
+    dispatch(fetchMe());
+    dispatch(fetchUsers());
+    dispatch(fetchTags());
+    const currentDate = moment().format('YYYY-MM');
+    dispatch(searchDiaries({ date: currentDate }));
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
