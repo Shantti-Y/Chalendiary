b1d7d679b73ecaebbdfdd5e8e080317163496133
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-06-24 22:51:30 +0900

    create emoji api

---
+++ src/main/kotlin/com/shanttiy/application/responsedata/propertydata/EmojiPropertydata.kt
@@ -0,0 +1,24 @@
+package com.shanttiy.application.responsedata.propertydata
+
+import com.shanttiy.application.usecaseboundary.ReplyUsecaseBoundary
+import com.shanttiy.application.usecaseboundary.UserUsecaseBoundary
+import com.shanttiy.domain.model.Diary
+import com.shanttiy.domain.model.Emoji
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+data class EmojiPropertydata (
+    val id: Int,
+    val value: String
+)
+
+@Component
+class EmojiPropertydataFactory(
+){
+    fun construct(emoji: Emoji): EmojiPropertydata{
+        return EmojiPropertydata(
+            id = emoji.id,
+            value = emoji.value
+        )
+    }
+}
