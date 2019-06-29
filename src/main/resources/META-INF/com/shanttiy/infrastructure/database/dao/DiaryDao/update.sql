ff680b147c900d1ec346a8f7265b16573a18ced5
Takahiro Yoshioka <tsolvalouy@gmail.com>
2019-06-29 16:36:17 +0900

    fix update query

--- src/main/resources/META-INF/com/shanttiy/infrastructure/database/dao/DiaryDao/update.sql
+++ src/main/resources/META-INF/com/shanttiy/infrastructure/database/dao/DiaryDao/update.sql
@@ -2,6 +2,7 @@ UPDATE
   diaries
 SET
   content_text = /* diaryEntity.contentText */'',
+  emoji_id = /* diaryEntity.emojiId */1,
   deleted_at = /* diaryEntity.deletedAt */NULL,
   updated_at = NOW()
 WHERE
